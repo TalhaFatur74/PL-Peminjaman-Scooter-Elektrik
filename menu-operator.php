7a9ece7a759299b9552fe02fbb13f6c36e965824
EKO PURWANTO <154056772+wawan-91@users.noreply.github.com>
2024-06-06 22:21:08 +0000

    menu operator pertama

--- menu-operator.php
+++ menu-operator.php
@@ -1,3 +1,37 @@
-<?php 
-include "config.php"; 
-?>
+<?php
+include "config.php";
+
+if (isset($_POST['keluar_menu_admin']))
+{
+    header("Location: index.php");
+    exit();
+}
+
+$tampilkanPesanBerhasil = false;
+$tampilkanPesanGagal = false;
+
+if (isset($_POST['action_penyewaan_belum_terdaftar']))
+{
+    $nomorKTP = $_POST['nomorKTP'];
+    $nomorScooter = $_POST['nomorScooter'];
+    $nama = $_POST['nama'];
+    $kelurahan = $_POST['kelurahan'];
+    $kecamatan = $_POST['kecamatan'];
+    $tanggalPenyewaan = $_POST['tanggalPenyewaan'];
+    $waktuMulai = $_POST['waktuMulai'];
+
+    $sql = "INSERT INTO penyewa VALUES('$nomorKTP', '$nama', '$kelurahan', '$kecamatan')";
+    $query = mysqli_query($db, $sql);
+
+    $sql2 = "INSERT INTO penyewaan VALUES(UUID(), '$nomorScooter', '$nomorKTP', '$tanggalPenyewaan', '$waktuMulai')";
+    $query2 = mysqli_query($db, $sql2);
+
+    if($query && $query2)
+    {
+        $tampilkanPesanBerhasil = true;
+    }
+    else
+    {
+        $tampilkanPesanGagal = true;
+    }
+}
