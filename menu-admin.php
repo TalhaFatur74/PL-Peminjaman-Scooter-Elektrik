20a3099a3964faac85c96c443ad99d49e82f3b10
TalhaFatur74 <148361554+TalhaFatur74@users.noreply.github.com>
2024-06-07 06:21:45 +0700

    Update menu-admin.php

--- menu-admin.php
+++ menu-admin.php
@@ -1,3 +1,53 @@
-<?php 
-include "config.php"; 
-?>
+<?php
+include "config.php";
+
+$tampilkanPesanBerhasil = false;
+$tampilkanPesanGagal = false;
+
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+    #MENU PENGELOLAAN DATA PENGGUNA
+    if (isset($_POST['action_pengguna'])) 
+    {
+        $action = $_POST['action_pengguna'];
+
+        if ($action == 'insert_pengguna')
+        {
+            $peran = $_POST['pilihanPeran'];
+            $kataSandi = $_POST['kataSandi'];
+
+            $sql = "INSERT INTO pengguna VALUES(UUID(), '$peran', '$kataSandi')";
+            $query = mysqli_query($db, $sql);
+
+            if($query)
+            {
+                $tampilkanPesanBerhasil = true;
+            }
+            else
+            {
+                $tampilkanPesanGagal = true;
+            }
+        }
+        else if($action == 'update_pengguna')
+        {
+            if (isset($_POST['update_filter'])) 
+            {
+                $update_filter = $_POST['update_filter'];
+
+                if($update_filter == 'update_id_pengguna')
+                {
+                    $idBaru = $_POST['id_baru'];
+                    $idPengguna = $_POST['idPengguna'];
+
+                    $sql = "UPDATE pengguna SET idPengguna = '$idBaru' WHERE idPengguna = '$idPengguna'"; 
+                    $query = mysqli_query($db, $sql);
+
+                    if($query)
+                    {
+                        $tampilkanPesanBerhasil = true;
+                    }
+                    else
+                    {
+                        $tampilkanPesanGagal = true;
+                    }
+                }
